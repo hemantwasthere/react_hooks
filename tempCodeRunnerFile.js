043557c5f2c50f8f5bc65a090ff451961a72fa55
Hemant <hrsmart2150@gmail.com>
2023-09-14 09:55:25 +0530

    Added tic tac toe

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-console.time("filterAndMap");
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const squaredOddNumbers = numbers.flatMap((num) =>
-  num % 2 !== 0 ? [num * num] : []
-);
-
-console.log(squaredOddNumbers); // [1, 9, 25, 49, 81]
-console.timeEnd("filterAndMap");
